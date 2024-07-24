c03b20c9d32062feed16641d3407239c586df349
Joshan Poudel <josanpoudel444@gmail.com>
2024-07-24 19:18:18 +0545

    modify view attendance page

--- lib/view/attendance/attendance.view.dart
+++ lib/view/attendance/attendance.view.dart
@@ -12,6 +12,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:nepali_utils/nepali_utils.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class AttendanceDetail extends StatefulWidget {
   final int courseSessionId;
@@ -26,9 +28,13 @@ class AttendanceDetail extends StatefulWidget {
 class _AttendanceDetailState extends State<AttendanceDetail> {
   Attendance? attendance;
   bool isRefreshing = false;
-  bool isUploading = false; // Added flag to track image upload status
+  bool isUploading = false;
   late Errors errors;
   final ImagePicker _picker = ImagePicker();
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  Map<DateTime, String> attendanceMap = {};
+  String errorMessage = '';
 
   @override
   void initState() {
@@ -36,17 +42,26 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
     fetchAttendances();
   }
 
-  Future<dynamic> fetchAttendances() async {
+  Future<void> fetchAttendances() async {
     setState(() {
       isRefreshing = true;
+      errorMessage = '';
     });
 
     try {
-      dynamic data =
-          await AttendanceAPIService().getAttendances(widget.courseSessionId);
+      dynamic data = await AttendanceAPIService().getAttendances(widget.courseSessionId);
+
       if (data is Attendance) {
         setState(() {
           attendance = data;
+          attendanceMap = {};
+          for (var record in attendance!.data) {
+            DateTime utcDate = DateTime.parse(record.date);
+            DateTime nepalDate = convertToNepalTime(utcDate);
+            DateTime dateKey = DateTime(nepalDate.year, nepalDate.month, nepalDate.day);
+
+            attendanceMap[normalizeDate(dateKey)] = record.status;
+          }
           isRefreshing = false;
         });
       } else if (data is Errors) {
@@ -57,9 +72,9 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
       }
     } catch (error) {
       generateErrorSnackbar("Error", "An unspecified error occurred!");
-    } finally {
       setState(() {
         isRefreshing = false;
+        errorMessage = 'An error occurred while fetching data.';
       });
     }
   }
@@ -74,7 +89,7 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
 
   Future<void> _uploadImage(File imageFile) async {
     setState(() {
-      isUploading = true; // Show the loader
+      isUploading = true;
     });
 
     try {
@@ -84,12 +99,12 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
       if (response.statusCode == 200) {
         generateSuccessSnackbar("Success", "Attendance taken successfully!");
         await fetchAttendances();
-      }else if(response.statusCode == 400){
-        generateErrorSnackbar("Error", "You are not allowed to take attendance or other students");
-      }else if(response.statusCode == 418){
+      } else if (response.statusCode == 400) {
+        generateErrorSnackbar(
+            "Error", "You are not allowed to take attendance for other students");
+      } else if (response.statusCode == 418) {
         generateErrorSnackbar("Error", "No student found!");
-      }
-       else {
+      } else {
         generateErrorSnackbar(
             "Error", "Failed to upload image: ${response.body}");
       }
@@ -98,18 +113,45 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
           "Attendance already taken today or an unspecified error occurred!");
     } finally {
       setState(() {
-        isUploading = false; // Hide the loader
+        isUploading = false;
       });
+      fetchAttendances();
     }
   }
 
-  String formatDate(String date) {
-    DateTime parsedDate = DateTime.parse(date);
-    return DateFormat.yMMMMd().format(parsedDate);
+  DateTime convertToNepalTime(DateTime utcDateTime) {
+    Duration offset = const Duration(hours: 5, minutes: 45);
+    return utcDateTime.add(offset);
+  }
+
+  DateTime normalizeDate(DateTime dateTime) {
+    return DateTime(dateTime.year, dateTime.month, dateTime.day);
+  }
+
+  void updateAttendanceForMonth(DateTime month) {
+    setState(() {
+      final startOfMonth = DateTime(month.year, month.month, 1);
+      final endOfMonth = DateTime(month.year, month.month + 1, 0);
+
+      attendanceMap = {};
+      for (var record in attendance!.data) {
+        DateTime utcDate = DateTime.parse(record.date);
+        DateTime nepalDate = convertToNepalTime(utcDate);
+        DateTime dateKey = DateTime(nepalDate.year, nepalDate.month, nepalDate.day);
+
+        if (dateKey.isAfter(startOfMonth.subtract(const Duration(days: 1))) &&
+            dateKey.isBefore(endOfMonth.add(const Duration(days: 1)))) {
+          attendanceMap[normalizeDate(dateKey)] = record.status;
+        }
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final sortedAttendanceEntries = attendanceMap.entries.toList()
+      ..sort((a, b) => b.key.compareTo(a.key));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -130,7 +172,7 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
         actions: [
           IconButton(
             onPressed: _openCamera,
-            icon: const Icon(Icons.add, color: Colors.white),
+            icon: const Icon(Icons.camera_alt_rounded, color: Colors.white),
           ),
         ],
       ),
@@ -138,56 +180,193 @@ class _AttendanceDetailState extends State<AttendanceDetail> {
         children: [
           RefreshIndicator(
             onRefresh: fetchAttendances,
-            child: attendance != null
-                ? ListView.builder(
-                    itemCount: attendance?.data.length,
-                    itemBuilder: (context, index) {
-                      var currentItem = attendance?.data[index];
-
-                      return Container(
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey,
-                              width: 1.0,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      TableCalendar(
+                        firstDay: DateTime.utc(2020, 1, 1),
+                        lastDay: DateTime.utc(2030, 12, 31),
+                        focusedDay: _focusedDay,
+                        calendarStyle: CalendarStyle(
+                          selectedDecoration: BoxDecoration(
+                            color: GlobalColors.mainColor,
+                            shape: BoxShape.circle,
+                          ),
+                          defaultTextStyle: const TextStyle(color: Colors.black),
+                          outsideTextStyle: const TextStyle(color: Colors.grey),
+                        ),
+                        headerStyle: HeaderStyle(
+                          titleTextStyle: TextStyle(color: GlobalColors.mainColor),
+                        ),
+                        daysOfWeekStyle: const DaysOfWeekStyle(
+                          weekendStyle: TextStyle(color: Colors.red),
+                          weekdayStyle: TextStyle(color: Colors.black),
+                        ),
+                        calendarBuilders: CalendarBuilders(
+                          defaultBuilder: (context, date, _) {
+                            final normalizedDate = normalizeDate(date);
+                            final attendanceStatus = attendanceMap[normalizedDate];
+                            Color backgroundColor;
+                            if (attendanceStatus == 'present') {
+                              backgroundColor = Colors.green;
+                            } else if (attendanceStatus == 'absent') {
+                              backgroundColor = Colors.red;
+                            } else {
+                              backgroundColor = Colors.transparent;
+                            }
+                            return Container(
+                              margin: const EdgeInsets.all(4.0),
+                              decoration: BoxDecoration(
+                                color: backgroundColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  date.day.toString(),
+                                  style: TextStyle(
+                                    color: attendanceStatus != null ? Colors.white : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          selectedBuilder: (context, date, _) {
+                            return Container(
+                              margin: const EdgeInsets.all(4.0),
+                              decoration: BoxDecoration(
+                                color: GlobalColors.mainColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  date.day.toString(),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            );
+                          },
+                          todayBuilder: (context, date, _) {
+                            final normalizedDate = normalizeDate(date);
+                            final attendanceStatus = attendanceMap[normalizedDate];
+                            Color backgroundColor;
+                            if (attendanceStatus == 'present') {
+                              backgroundColor = Colors.green;
+                            } else if (attendanceStatus == 'absent') {
+                              backgroundColor = Colors.red;
+                            } else {
+                              backgroundColor = Colors.transparent;
+                            }
+                            return Container(
+                              margin: const EdgeInsets.all(4.0),
+                              decoration: BoxDecoration(
+                                color: backgroundColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  date.day.toString(),
+                                  style: TextStyle(
+                                    color: attendanceStatus != null ? Colors.white : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        onDaySelected: (selectedDay, focusedDay) {
+                          setState(() {
+                            _selectedDay = selectedDay;
+                            _focusedDay = focusedDay;
+                          });
+                        },
+                        onPageChanged: (focusedDay) {
+                          setState(() {
+                            _focusedDay = focusedDay;
+                            updateAttendanceForMonth(focusedDay);
+                          });
+                        },
+                        enabledDayPredicate: (day) {
+                          return true;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Attendance Record',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: GlobalColors.mainColor,
+                          ),
+                        ),
+                      ),
+                      if (errorMessage.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            errorMessage,
+                            style: const TextStyle(
+                              color: Colors.red,
+                              fontSize: 14,
                             ),
                           ),
                         ),
-                        margin: const EdgeInsets.all(15.0),
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              formatDate(currentItem!.date),
-                              style: const TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 10),
-                            GreyText(
-                                text: "Student: ${currentItem.studentName}"),
-                            const SizedBox(height: 10),
-                            GreyText(
-                                text:
-                                    "Status: ${titleCase(currentItem.status)}"),
-                          ],
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: sortedAttendanceEntries.length,
+                          itemBuilder: (context, index) {
+                            DateTime date = sortedAttendanceEntries[index].key;
+                            String status = sortedAttendanceEntries[index].value;
+                            return ListTile(
+                              leading: Icon(
+                                status == 'present'
+                                    ? Icons.check_circle
+                                    : Icons.cancel,
+                                color: status == 'present' ? Colors.green : Colors.red,
+                              ),
+                              title: Text(
+                                DateFormat('yyyy-MM-dd').format(date),
+                                style: const TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Status: ${titleCase(status)}',
+                                style: TextStyle(
+                                  color: status == 'present' ? Colors.green : Colors.red,
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
-                  )
-                : isRefreshing
-                    ? Center(
-                        child: ModernSpinner(color: GlobalColors.mainColor))
-                    : const Center(
-                        child: Text("No attendance records found!"),
                       ),
+                    ],
+                  ),
+                ),
+                if (isRefreshing)
+                  Center(child: ModernSpinner(color: GlobalColors.mainColor))
+                else if (attendance == null || attendance!.data.isEmpty)
+                  Center(
+                    child: Text(
+                      'No attendance records found!',
+                      style: TextStyle(
+                        color: GlobalColors.textColor,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
           if (isUploading)
             Container(
-              color: Colors.black.withOpacity(0.5),
-              child: Center(
-                child: ModernSpinner(
-                  color: GlobalColors.mainColor,
+              color: Colors.black54,
+              child: const Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                 ),
               ),
             ),
